4f28a2e9442cd38255877a222893ec0e95833fa7
Arturo Ocampo <arturoocampo@byui.edu>
2025-10-06 14:55:08 +0100

    Adjustment Swagger.js

--- swagger.js
+++ swagger.js
@@ -1,22 +1,37 @@
 // swagger.js
 const swaggerAutogen = require('swagger-autogen')();
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const doc = {
   info: {
     title: 'CSE341 API - Week 4',
     description: 'Contacts API'
   },
-  host: isProd ? 'cse341-week4-4b58.onrender.com' : 'localhost:8080',
-  schemes: [isProd ? 'https' : 'http'],
+  // No se define host ni schemes: Swagger UI usará el origen actual (Render o localhost)
   basePath: '/',
   consumes: ['application/json'],
-  produces: ['application/json']
+  produces: ['application/json'],
+  tags: [
+    { name: 'Contacts', description: 'Operations for contacts' }
+  ],
+  definitions: {
+    Contact: {
+      firstName: 'Arturo',
+      lastName: 'Ocampo',
+      email: 'arturo@example.com',
+      favoriteColor: 'Blue',
+      birthday: '1990-05-20'
+    },
+    ContactInput: {
+      $firstName: 'Arturo',
+      $lastName: 'Ocampo',
+      $email: 'arturo@example.com',
+      favoriteColor: 'Blue',
+      birthday: '1990-05-20'
+    }
+  }
 };
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
 
-// Genera swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
